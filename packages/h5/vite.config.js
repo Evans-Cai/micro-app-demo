723c07f6defa25f1716422890dacc1725ebe6d83
bobevans <caiwendi88@gmail.com>
2024-03-04 14:53:48 +0800

    Update vite.config.js

--- packages/h5/vite.config.js
+++ packages/h5/vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3003,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS, HEAD',
+      'Access-Control-Allow-Headers': 'X-Requested-With, Content-Type, Accept'
+    }
+  }
 })
